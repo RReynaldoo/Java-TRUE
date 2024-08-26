e9c1b42af6be7f7825a03edf87f449599d7ba6ed
Reynaldo <157304105+RReynaldoo@users.noreply.github.com>
2024-08-26 13:34:28 -0400

    First

---
+++ 1PrintAndComments.java
@@ -0,0 +1,11 @@
+public class Name {
+	public static void main(String[]args)
+	{
+		System.out.println("This prints something");	//Regular print but the next print will be on the next line
+		System.out.print("This prints something/n");	//Same but manually
+		
+		//To add any character like ^ use slash or look for a list of commands for that
+		 	
+	}
+
+}
