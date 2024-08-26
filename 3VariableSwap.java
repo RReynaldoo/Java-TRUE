7a4b917731c82b9aededd5da842ad9270a39fa94
Reynaldo <157304105+RReynaldoo@users.noreply.github.com>
2024-08-26 13:55:27 -0400

    Third

---
+++ 3VariableSwap.java
@@ -0,0 +1,17 @@
+public class Name {
+	public static void main(String[]args)
+	{
+		String name1 = "Jhosh";
+		String name2 = "Thomas";
+		String temp;
+		
+		temp=name1;
+		name1=name2;
+		name2=temp;
+		
+		System.out.println(name1);
+		System.out.println(name2);
+		 	
+	}
+
+}
