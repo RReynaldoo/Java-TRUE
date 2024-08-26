cc6254e86eebd59271f84610ed6b5af0363f1888
Reynaldo <157304105+RReynaldoo@users.noreply.github.com>
2024-08-26 13:49:58 -0400

    Second

---
+++ 2Variables.java
@@ -0,0 +1,31 @@
+public class Name {
+	public static void main(String[]args)
+	{
+		//You can create a variable using the first two or the third one
+		
+		int x;		//declaration
+		x = 12;		//assignment
+		int y = 12;	//initialization
+		
+		//------------------------------------------------------------
+		
+		System.out.println("A number is "+y);	//To display a variable
+		
+		//------------------------------------------------------------
+		// boolean < byte < short < int < long < float < double < char (Storage)
+		
+		boolean b = true;
+		byte B = 12;
+		short a = 333;
+		int A = 333;
+		long h = 888L;
+		
+		float w = 77.9f;
+		double g = 3333.4;
+		
+		char t = 't';			//Stores anything between '' as character
+		String name = "Thomas";	//Stores anything between "" as String
+		 	
+	}
+
+}
