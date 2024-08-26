eb0c3562e7888b9e2345f35cb2e677204c16d8bd
Reynaldo <157304105+RReynaldoo@users.noreply.github.com>
2024-08-26 14:45:12 -0400

    fifth

---
+++ 5Operands.java
@@ -0,0 +1,16 @@
+public class Name {
+	public static void main(String[]args)
+	{
+		 int x = 13;
+		 
+		 x = x + 2;	//add
+		 x = x - 2;	//sub
+		 x = x * 2;	//multiplication
+		 x = x / 2;	//division
+		 x = x % 2;	//reminder
+		 
+		 x++;	//add 1
+		 x--;	//sub 1
+	}
+
+}
