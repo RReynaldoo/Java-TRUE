146dd76ea17dcf5baf9d40e8f679a42178e9df17
Reynaldo <157304105+RReynaldoo@users.noreply.github.com>
2024-08-27 14:04:36 -0400

    arrays

--- 11.Strings.java
+++ 11.Strings.java
@@ -2,3 +2,4 @@
 
 name.isBlank();     //Check if the string is ""
 name.equals();      //checks if the string is equal to the value in parenthesis
+name.length();      //Checks the length of the string or string[]
---
+++ 15Array.java
@@ -0,0 +1,15 @@
+//arrays = used to store multiple values in a single variable
+import java.util.Scanner;
+public class Name {
+	public static void main(String[]args)
+	{
+		String[] names = new String[3];
+		
+		names[0] = "Louise";
+		names[1] = "Mike";
+		names[2] = "Charles";
+		
+		
+	}
+
+}
