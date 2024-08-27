58144211423a964aebdc3ef54b24eee34bae782a
Reynaldo <157304105+RReynaldoo@users.noreply.github.com>
2024-08-27 13:03:18 -0400

    aa

---
+++ 11.Strings.java
@@ -0,0 +1,4 @@
+//These are string methods, there are many that can be accesed by a dot next to a string
+
+name.isBlank();     //Check if the string is ""
+name.equals();      //checks if the string is equal to the value in parenthesis
