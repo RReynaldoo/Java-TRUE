88b6073f96f04aa45f0fd3ffa6ce08f7a1c6dc7e
Reynaldo <157304105+RReynaldoo@users.noreply.github.com>
2024-08-27 13:13:22 -0400

    twelve

---
+++ 12WhileLoops.java
@@ -0,0 +1,22 @@
+//while loop = executes a block of code as long as a condition remains true
+import java.util.Scanner;
+public class Name {
+	public static void main(String[]args)
+	{
+		Scanner input = new Scanner(System.in);
+		String name = "";
+//############################################# Checks the condition before the block		
+		while(name.isBlank());
+		{
+			//Do something
+		}
+//############################################# Checks the condition afterwards
+		do
+		{
+			//Do something
+		}
+		while(name.isBlank());
+//#############################################
+	}
+
+}
