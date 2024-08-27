c48794ed31ce0da4dd1d75c26fad87ad73f57ba8
Reynaldo <157304105+RReynaldoo@users.noreply.github.com>
2024-08-27 12:22:07 -0400

    tenth

---
+++ 10Switch.java
@@ -0,0 +1,35 @@
+//Switch = statement that allows a variable to be tested for equality against a list of values
+public class Name {
+	public static void main(String[]args)
+	{
+				//Strings
+		int x = 4;
+		String day = "Monday";
+		
+		switch(day)
+		{
+		case"Monday":
+			//Do something
+			break;
+		case "Friday":
+			//Do something
+			break;
+		default:
+			//Do something if everything else is false;
+		}
+		
+				//Numbers
+		switch(x)
+		{
+		case(1):
+			//Do something
+			break;
+		case(3):
+			//Do something
+			break;
+		default:
+			//Do something if everything else is false;
+		}
+	}
+
+}
