31f1dc5ac3b050bb07b137a46338fb8ed2027aa9
Reynaldo <157304105+RReynaldoo@users.noreply.github.com>
2024-08-27 12:12:38 -0400

    nineth

---
+++ 9IfElse.java
@@ -0,0 +1,23 @@
+public class Name {
+	public static void main(String[]args)
+	{
+		int age = 18;
+		
+		if(age == 12)
+		{
+			//Do something
+		}
+		else if(age < 12)
+		{
+			//Do another thing
+		}
+		else 
+		{
+			//One last thing
+		}
+		
+		//You can also check groups of conditions with () || && 
+		//You can also nest them
+	}
+
+}
