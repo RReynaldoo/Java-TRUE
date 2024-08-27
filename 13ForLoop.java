7e0375258b9cc4b0a50fce390f4d730779823eb7
Reynaldo <157304105+RReynaldoo@users.noreply.github.com>
2024-08-27 13:30:44 -0400

    maaan I shoulda buy more snacks

---
+++ 13ForLoop.java
@@ -0,0 +1,13 @@
+//for loop = executes a block of code a limited amount of times
+import java.util.Scanner;
+public class Name {
+	public static void main(String[]args)
+	{
+		//Initialize variable; condition; modify value
+		for(int i = 10; i >= 0; i--)
+		{
+			System.out.println(i);
+		}
+	}
+
+}
