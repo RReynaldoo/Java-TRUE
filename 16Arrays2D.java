3f99058e8d37dbb1f122a4660cccd40a8b902fcf
Reynaldo <157304105+RReynaldoo@users.noreply.github.com>
2024-08-27 14:19:41 -0400

    2d

---
+++ 16Arrays2D.java
@@ -0,0 +1,21 @@
+//2d arrays = an array of array
+import java.util.Scanner;
+public class Name {
+	public static void main(String[]args)
+
+	{
+//##################################################    assignment one by one
+		String[][] names = new String[3][3];
+		names[0][0] = "Louise";
+		names[0][1] = "Lucy";
+		names[0][2] = "Luke";
+//##################################################    assigment in declaration
+		String[][] names2 = {
+							{"one","two","Three"},
+							{"one","two","Three"},
+							{"uno","dos","tres"}
+							};
+		
+	}
+
+}
