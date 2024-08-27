cb4bace6041ed4e09936ade828621a23ae63e0ed
Reynaldo <157304105+RReynaldoo@users.noreply.github.com>
2024-08-27 14:42:15 -0400

    things

--- 11.Strings.java
+++ 11.Strings.java
@@ -1,5 +1,12 @@
 //These are string methods, there are many that can be accesed by a dot next to a string
 
 name.isBlank();     //Check if the string is ""
-name.equals();      //checks if the string is equal to the value in parenthesis
-name.length();      //Checks the length of the string or string[]
+name.inEmpty();     //Check if the string is ""
+name.equals();      //checks if the string is equal to the value in parenthesis     BOOLEAN
+name.length();      //Checks the length of the string or string[]                   INTEGER
+name.charAt(0);     //Returns the character in the given index                      CHAR
+name.indexOf("y");  //Returns the index of the given char                           INTEGER
+name.toUpperCase();
+name.toLowerCase();
+name.trim();        //Gets rid of empty spaces
+name.replace(x,y);
---
+++ 18WrapperClass.java
@@ -0,0 +1,15 @@
+// wrapper class =  provides a way to use primitive data types as reference data types
+import java.util.Scanner;
+public class Name {
+	public static void main(String[]args)
+	{
+//  Wrapper version				primitive version
+	Boolean a = true;			boolean A = true;
+	Character b = '@';			char B = '@';
+	Integer c = 123;			int C = 123;
+	Double d = 3.14;			double D = 3.14;
+//	Difference is that the wrapper version have access to methods like string has and can be also used as primitive
+//	They are slower
+	}
+
+}
