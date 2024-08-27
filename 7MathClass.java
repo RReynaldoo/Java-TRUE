85a00069fd94955fc773d0d0f6ea85809d4d508c
Reynaldo <157304105+RReynaldoo@users.noreply.github.com>
2024-08-27 11:54:48 -0400

    seventh

---
+++ 7MathClass.java
@@ -0,0 +1,17 @@
+public class Name {
+	public static void main(String[]args)
+	{
+		double x = 3.14;
+		double y = -12;
+		
+		//These are some useful methods of math class, there are plenty more
+		Math.abs(x);
+		Math.max(x, y);
+		Math.min(x, y);
+		Math.sqrt(x);
+		Math.round(x);
+		Math.ceil(x);
+		Math.floor(x);
+	}
+
+}
